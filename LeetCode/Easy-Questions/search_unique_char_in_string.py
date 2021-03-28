51733126e12094ccbbf0ac2c2147add580a43a83
Edim Meduseljac <57595175+edimmeduseljac@users.noreply.github.com>
2021-03-28 19:59:28 +0200

    Unique character
    Easy question on LeetCode - Analyzing, searching and finding the one unique character inside a defined string. Result is then printed to the console.

---
+++ LeetCode/Easy-Questions/search_unique_char_in_string.py
@@ -0,0 +1,26 @@
+s = "aabbccd"
+
+checkingArray = []
+index = 0
+for i in s:
+    if len(checkingArray) == len(s) and checkingArray[index] != i:
+        checkingArray.clear()
+    else: 
+        checkingArray.append(i)
+        index += 1
+
+index = 0
+v = 1
+unique = ""
+for i in range(len(checkingArray)):
+    if checkingArray.count(checkingArray[index]) > 1:
+        if index < len(checkingArray) - 1 :
+            index += 1
+        else: 
+            print(f"return {-1}")
+            break
+    elif checkingArray.count(checkingArray[index]) == 1: 
+        unique = checkingArray[index]
+        print(f"return {checkingArray.index(checkingArray[index])}")
+        break
+
