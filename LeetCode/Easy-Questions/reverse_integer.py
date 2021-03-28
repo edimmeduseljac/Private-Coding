cb76d8740e19b6ba20c0557b0ec13dfff04c19eb
Edim Meduseljac <57595175+edimmeduseljac@users.noreply.github.com>
2021-03-28 19:56:27 +0200

    Reversing an integer
    Easy-Question on LeetCode - Reversing an Integer and printing the result to the console

---
+++ LeetCode/Easy-Questions/reverse_integer.py
@@ -0,0 +1,19 @@
+x = int(input("Enter the number you want to reverse: "))
+
+class Solution:
+    def reverseInteger():
+        v = 0
+        number = 0
+        if x > 0: 
+            a = str(x)
+            for i in a: 
+                number = number + int(i) * pow(10,v)
+                v += 1
+        else: 
+            n = x - (2*x)
+            a = str(n)
+            for i in a: 
+                number = number + int(i) * pow(10,v) * (-1)
+                v += 1
+        print(number)
+    reverseInteger()
