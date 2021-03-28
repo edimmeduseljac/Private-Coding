1e01e540abedb60b69513580a8674b742fb12453
Edim Meduseljac <57595175+edimmeduseljac@users.noreply.github.com>
2021-03-28 20:04:22 +0200

    Validating palindrome
    Easy question on LeetCode - Checking if a string is a palindrome, by first cleaning the string up if there are any special characters (e.g. " , ; : [ ]), then reversing the string and validating if the given string is a palindrome or not.

---
+++ LeetCode/Easy-Questions/validate_if_palindrome.py
@@ -0,0 +1,12 @@
+s = "A man, a plan, a canal: Panama"
+
+class Solution: 
+    def validatePalindrome():
+        cleanedS = s.replace(",","").replace(" ","").replace(":","").lower()
+        reversedCleanedS = cleanedS[::-1]
+
+        if cleanedS == reversedCleanedS:
+            print("true")
+        else: 
+            print("false")
+    validatePalindrome()
